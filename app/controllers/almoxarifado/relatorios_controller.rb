1c09710f115e24419b47b4522cfe4091c280ef0e
Cassio Almeida <jcassiocosta@gmail.com>
2019-02-04 16:59:01 -0300

    simplificado relatorios do almoxarifado

--- app/controllers/almoxarifado/relatorios_controller.rb
+++ app/controllers/almoxarifado/relatorios_controller.rb
@@ -1,7 +1,6 @@
 class Almoxarifado::RelatoriosController < ApplicationController
   def index
-  	@entradas = Almoxarifado::Entrada.group_by_day(:created_at, format: "%d/%m/%Y").count
-  	@retiradas = Almoxarifado::Retirada.group_by_day(:created_at, format: "%d/%m/%Y").count
-  	@materiais = Almoxarifado::Material.group(:descricao).count
+  	@entradas = Almoxarifado::Entrada.group_by_week(:created_at, format: "%d/%m/%Y").count
+  	@retiradas = Almoxarifado::Retirada.group_by_week(:created_at, format: "%d/%m/%Y").count
   end
 end
